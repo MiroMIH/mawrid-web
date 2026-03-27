d26c3644ca306ebac314fa71bf2ca8fe4d1e3ed1
Miro <amirbelaifa2001@gmail.com>
2026-03-27 17:35:57 +0100

    initial commit

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'sonner';
 import { useAuthStore } from '../store/authStore';
 
-const BASE_URL = 'http://localhost:8080/api/v1';
+const BASE_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:8080/api/v1';
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
